1b6b10a9332a569189621d11a2769c65e0d989ba
JanineEgert <janine.egert@zbsa.de>
2018-04-05 11:04:02 +0100

    Save current O in xls

--- Subfunctions/OmicsWrite.m
+++ Subfunctions/OmicsWrite.m
@@ -12,16 +12,27 @@ end
 
 data = get(O,'data');
 SampleNames = get(O,'SampleNames');
-ProteinNames = get(O,'Proteinnames');
+if isfield(O,'Proteinnames')
+    ProteinName = get(O,'Proteinnames');
+elseif isfield(O,'Proteinname')
+    ProteinName = get(O,'Proteinname');
+elseif isfield(O,'ProteinIDs')
+    ProteinName = get(O,'ProteinIDs');
+end
 
 if nargin>3 && ~isempty(varargin)
-    for i=1:2:size(varargin)
+    for i=1:2:size(varargin,2)
         if isstring(varargin{i}) || ischar(varargin{i})
-          SampleNames = [SampleNames, varargin(i)];
+            for j=1:size(varargin{i+1},2)
+                SampleNames = [SampleNames, varargin(i)];
+            end
         else
             error('OmicsWrite.m: Expected string as input. Each extra variable has to be named first.')
         end
         if isnumeric(varargin{i+1}) && ( size(varargin{i+1},1) == size(data,1) )
+            data = [data, varargin{i+1}];
+        elseif isnumeric(varargin{i+1}) && (size(varargin{i+1},1) < size(data,1))
+            varargin{i+1} = [ varargin{i+1}; nan(size(data,1)-length(varargin{i+1}) , size(varargin{i+1},2) ) ];
             data = [data, varargin{i+1}];
         else
             error('OmicsWrite.m: Expected vector of size(dat,1) to define a Protein parameter.')
@@ -30,7 +41,7 @@ if nargin>3 && ~isempty(varargin)
 end
 
 xlswrite(outfile,SampleNames,'Sheet1','B1');
-xlswrite(outfile,ProteinNames,'Sheet1','A2');
+xlswrite(outfile,ProteinName,'Sheet1','A2');
 xlswrite(outfile,data,'Sheet1','B2');
 
 if nargin>3 && ~isempty(varargin)
@@ -38,7 +49,12 @@ if nargin>3 && ~isempty(varargin)
     for i=1:2:size(varargin)
         fprintf([', ' varargin{i}])
     end
-    sprintf([' columnwise in ' outfile '.\n'])
+    fprintf([' columnwise in ' outfile '.\n'])
 else
     fprintf(['OmicsWrite.m wrote data matrix in ' outfile '.\n'])
 end
+
+% if isfield(O,'X')
+%     X = get(O,'X');
+%     save('Designmatrix.mat','X')
+% end
