73db663c725a4f9ea1572b251670d4e3bdcef71c
JanineEgert <janine.egert@zbsa.de>
2018-04-24 12:27:28 +0100

    Overview of data

--- MatlabTools/plotdata.m
+++ MatlabTools/plotdata.m
@@ -1,16 +1,28 @@
+% Data overview
 
-function plotdata(O)
+function plotdata
+
+global O
 
 dat = get(O,'data');
+SampleNames = get(O,'SampleNames');
+
+figure
+imagesc(dat)
+title('Data matrix')
+
+figure
+imagesc(isnan(dat))
+title('Missing values in data')
+
 figure
 plot(log10(dat),'o')
 xlabel('Protein')
 ylabel('data value')
-legend('measurements','hellblau und rot sind nicht light','liegt an niedriger Tagesmessung','--> Quantil Norm')
 
 figure
 plot(log10(dat'))
 xlabel('Measurement')
 ylabel('data value')
-% legend('Proteins','Arianes Quantil normalisierung?')
+legend(SampleNames,'Location','eastoutside')
 
