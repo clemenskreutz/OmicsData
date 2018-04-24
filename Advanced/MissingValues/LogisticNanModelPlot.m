942e750bbbef2bceb0634e71d22a92cf4c46ba64
JanineEgert <janine.egert@zbsa.de>
2018-04-24 12:14:24 +0100

    Plot logistic regression of matrix pattern

--- Advanced/MissingValues/LogisticNanModelPlot.m
+++ Advanced/MissingValues/LogisticNanModelPlot.m
@@ -1,5 +1,13 @@
 
-function LogisticNanModelPlot(out)
+function LogisticNanModelPlot
+
+global O
+
+if ~exist('O','var')
+    error('MissingValues/LogisticNanModel.m requires class O as global variable or input argument.')
+end
+
+out = get(O,'out');
 
 t = out.type(:,1);
 b1 = out.b(t==1,:);
@@ -8,13 +16,36 @@ b3 = out.b(t==3,:);
 b3s = sort(mean(b3,2));
 
 figure
-boxplot(b2')
+boxplot(b1)
+title('Intensity coefficient')
+ylabel('Intensity Coefficient')
+
 figure
-subplot(1,2,1)
+
+boxplot(b2')
+title('Coefficients per experiment')
+xlabel('Experiments')
+ylabel('Regression coefficients')
+
+figure
+suptitle('Coefficients per Protein')
+subplot(1,3,1)
 plot(1:length(b3),b3s)
 xlabel('# of row')
-ylabel('b: log reg coeff for row')
-subplot(1,2,2)
+ylabel('b: log reg coeff for row (sorted)')
+subplot(1,3,2)
 histogram(b3s)
 xlabel('b')
 ylabel('Frequency')
+
+% Probability
+logit = exp(b3s);
+p = 1./(1./logit+1); 
+
+
+subplot(1,3,3)
+histogram(p)
+xlabel('p (missing values)')
+ylabel('Frequency')
+
+
