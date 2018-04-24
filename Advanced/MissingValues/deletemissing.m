e46fbf57adede0cc54bf3759df3fe5912f35c0b2
JanineEgert <janine.egert@zbsa.de>
2018-04-24 12:16:46 +0100

    deletes all rows with at least one missing value

--- Advanced/MissingValues/deletemissing.m
+++ Advanced/MissingValues/deletemissing.m
@@ -1,38 +1,34 @@
 
-% O = deletemissing(O)
-% 
-%  Just data without missing values is used
+%  deletemissing
 %  delete all lines with at least one missing value
 
-function O = deletemissing(O,missing)
+function deletemissing
 
-if ~exist('missing','var')
-    missing = [];
+global O
+
+if ~exist('O','var')
+    error('MissingValues/LogisticNanModel.m requires class O as global variable or input argument.')
 end
 
 dat = get(O,'data');
 
-if missing == 0                          % delete all zero elements
-    [idx,~] = find(~dat);              % find looks for nonzero elements
-elseif strcmp(missing,'mix')
-    [idx,~] = find(isnan(dat));        % delete all zero elements AND all nan elements
-    [idx2,~] = find(~dat);               % in example dataset02 really a mixture exists
-    idx = [idx idx2];
-else
-    [idx,~] = find(isnan(dat));       % delete all nan elements
-end                                     
+%% Remember original dataset
+O = set(O,'data_original',[]);          % Put in container so it stays original (always same size)  
+O = set(O,'data_original',dat,'Original dataset.');
+
+%% Delete all nan elements
+[idx,~] = find(isnan(dat));                               
 idx = unique(idx);
 rows = 1:size(dat,1);
 comp = setdiff(rows,idx);
+O = O(comp,:);
 
+%% Output message
 if isempty(idx)
     warning('Data is not changed. There is no line without a missing and/or zero value.\n');
 elseif isempty(comp)
     warning('Data is not changed. All lines have a missing and/or zero value.\n')
-else
-    O = set(O,'data_original',dat,'Original dataset.');
-    O = O(comp,:); 
+else    
     warning('All lines with missing values deleted.\n')   
-
 end
 
