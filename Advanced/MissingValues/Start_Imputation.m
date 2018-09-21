b18f555aac1f44b5da44300a922115e98d4fa8a3
JanineEgert <janine.egert@zbsa.de>
2018-09-21 16:12:37 +0100

    Starting file for imputation

--- Advanced/MissingValues/Start_Imputation.m
+++ Advanced/MissingValues/Start_Imputation.m
@@ -1,37 +1,48 @@
 clear all
 close all
-
 global O
 
-%% Load data
-%file = 'Data/dataset01_yeast_oxICAT.xlsx';
-%file = 'Data/dataset02_tbrucei_ATOM_depletome_proteins_christian.txt';
-file = 'Data/dataset03_mouse_comparison_of_cell_lines_proteins_lena.xlsx';
+%% File
+file = 'Data/dataset01_yeast_oxICAT.xlsx';
+%file = 'Data/dataset02_tbrucei_ATOM_depletome_proteins_christian.txt'; % 51 rows completely missing, other rows non at all missing
+%file = 'Data/dataset03_mouse_comparison_of_cell_lines_proteins_lena.xlsx';
 
-O = OmicsData(file);
-
-if  strcmp(file,'Data/dataset02_tbrucei_ATOM_depletome_proteins_christian.txt')
-    O = O(:,1:size(get(O,'data'),2)-1);
+% Load data
+[filepath,name] = fileparts(file);
+if exist([filepath '/' name '/O.mat'],'file') 
+    load([filepath '/' name '/O.mat']) 
+else
+    error('You first have to run Start_MissingPattern.m')
 end
-if  strcmp(file,'Data/dataset03_mouse_comparison_of_cell_lines_proteins_lena.xlsx')
-    O = O(:,7:size(get(O,'data'),2));
-end
 
-% Wrote it here so everybody sees it. Sometimes 0s are missing/notmeasured values. Sometimes they are just 0.
-% Look at your data. And Uncomment if so.
-dat = get(O,'data');
-dat(dat==0) = nan;  
-O = set(O,'data',dat,'Replaced 0 by nan.')
 
-%% Plot overview of data matrix
-%plotdata
-    
-%% Evaluate missing pattern
-analysemissing
+%% Delete previous imputations ?
+imputation_clear
 
-%% imputation
-T = imputation;
+%% DMwR
+lib= 'DMwR';
+methods = 'knn';
+impute_R(lib,methods);
 
-%% save output
-% output(O,file,'NotSorted');
+%% Hmisc
+lib= 'Hmisc';
+methods = {'pmm','regression'};
+impute_R(lib,methods);
 
+%% MICE
+lib= 'mice';
+%methods = 'cart';
+methods = {'pmm','midastouch','sample','cart','rf','mean','norm','ri'};
+impute_R(lib,methods)
+
+%% Amelia
+lib= 'Amelia';
+impute_R(lib);
+
+%% missForest
+lib= 'missForest';
+impute_R(lib);
+
+
+%% Plot
+imputation_analysis;
