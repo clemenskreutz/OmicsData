72bc39ce4de817450a50499b98cb4813ddf0c07b
unknown <janine.egert@zbsa.uni-freiburg.de>
2019-02-07 12:54:50 +0000

    Update Rpackages

--- Advanced/MissingValues/imputation.m
+++ Advanced/MissingValues/imputation.m
@@ -1,46 +1,43 @@
-function imputation(path,clean)
+function imputation(clean)
 
 global O
 
-if ~exist('path','var')
-    path = [];
-end
 if ~exist('clean','var') || clean
     imputation_clear  % clear previous imputations in O, optional
 end
 
 lib= 'mice';
 methods = {'mean','norm','ri','pmm','sample','cart'};
-impute_R(lib,methods,path)
+impute_R(lib,methods)
 
 lib= 'mice';
 methods = {'midastouch','rf'};
-impute_R(lib,methods,path)
+impute_R(lib,methods)
 
 lib= 'Amelia';
 impute_R(lib,[],[]);
 
 lib= 'Hmisc';
 methods = {'regression'};
-impute_R(lib,methods,path);
+impute_R(lib,methods);
 
 lib= 'pcaMethods';
 methods = {'ppca','bpca','nipals','svd','svdImpute'};
-impute_R(lib,methods,path);
+impute_R(lib,methods);
 
 lib= 'imputeLCMD';
 methods = {'MinDet','KNN','MinProb','QRILC'};
-impute_R(lib,methods,path);
+impute_R(lib,methods);
 
 lib= 'imputation';
 methods = {'SVTImpute','SVDImpute','kNNImpute'};  %,'SVTApproxImpute','lmImpute'
-impute_R(lib,methods,path);
+impute_R(lib,methods);
 
 lib= 'missForest';
-impute_R(lib,[],[]);
+impute_R(lib,[]);
 
 lib= 'softImpute';
-impute_R(lib,[],[]);
+impute_R(lib,[]);
 
 lib= 'VIM';
 methods = {'irmi'}; %'kNN'
