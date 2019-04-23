9c43cd566d7709a9f617582cbb79c811dfad9e7d
clemenskreutz <ckreutz@fdm.uni-freiburg.de>
2019-04-23 15:54:16 +0200

    Subsindex is now used for subsetting to ensure validity of all rows and data fields

--- Subfunctions/OmicsReducebyName.m
+++ Subfunctions/OmicsReducebyName.m
@@ -1,6 +1,5 @@
 
 function O = OmicsReducebyName(O,name)
-
 % e.g. O = ReducebyName(O,'A_')
 % Reduces dataset to SampleNames which include 'A_'
 
@@ -8,14 +7,5 @@ if nargin<2
     error('OmicsData/ReducebyName.m requires at least 2 input arguments. ReducebyString(O,name).')
 end
 
-dat = get(O,'data');
-Names = get(O,'SampleNames');
-
-reducedata = [];
-for i=1:length(Names)
-    if strfind(Names{i},name)
-        reducedata = [reducedata i];
-    end
-end
-dat = dat(:,reducedata);
-O = set(O,'data',dat,['Reduced by name (' name ')']);
+drin = find(~cellfun(@isempty,strfind(get(O,'snames'),name)));
+O = O(:,drin);
