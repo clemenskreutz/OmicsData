14b420efe516b5fc424976156484eb71376287ae
clemenskreutz <ckreutz@fdm.uni-freiburg.de>
2019-04-23 16:04:03 +0200

    Comment added, more reasonable variable name

--- Subfunctions/OmicsReducebyName.m
+++ Subfunctions/OmicsReducebyName.m
@@ -1,11 +1,17 @@
+% O = ReducebyName(O,pattern)
+% 
+% Filtering of the dataset to SampleNames which include a pattern.
+% 
+%   pattern     a pattern which is searched via strfind in the SampleNames
+% 
+% Example:
+% O = OmicsReducebyName(O,'A_')
 
-function O = OmicsReducebyName(O,name)
-% e.g. O = ReducebyName(O,'A_')
-% Reduces dataset to SampleNames which include 'A_'
+function O = OmicsReducebyName(O,pattern)
 
 if nargin<2
     error('OmicsData/ReducebyName.m requires at least 2 input arguments. ReducebyString(O,name).')
 end
 
-drin = find(~cellfun(@isempty,strfind(get(O,'snames'),name)));
+drin = find(~cellfun(@isempty,strfind(get(O,'snames'),pattern)));
 O = O(:,drin);
