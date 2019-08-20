4449c22a2c2010a41a877318107b444fc5f40e34
JanineEgert <egert@imbi.uni-freiburg.de>
2019-08-20 13:53:28 +0200

    Start script

--- Advanced/MissingValues/Start_Lena.m
+++ Advanced/MissingValues/Start_Lena.m
@@ -5,7 +5,4 @@ O = OmicsData(file);                         % Write in class O
 O = log2(O);   
 O = O(1:500,1:5);
 
-%% Simulate pattern
-O = set(O,'deleteemptyrows',true);           % delete rows with all nan
-O = set(O,'npat',5);						 % how many pattern simulations
 O = DIMA(O);
