f49c4eaace07839299b046102d537dac6567c72c
clemenskreutz <ckreutz@fdm.uni-freiburg.de>
2019-08-23 20:03:19 +0200

    Seed as addiontal argument in order ensure reproducibility

--- OmicsMice.m
+++ OmicsMice.m
@@ -1,4 +1,4 @@
-% OmicsMice(O,[method])
+% OmicsMice(O,[method],[seed])
 %
 %   This function generates a single realization of the mice imputation
 %   algorithm.
@@ -8,11 +8,17 @@
 %         pmm           any     Predictive mean matching
 %                   see mice.m for further details
 % 
+%       seed is set by default to a fixed number in order ensure
+%            reproducility
+% 
 % See also mice
 
-function O = OmicsMice(O,method)
+function O = OmicsMice(O,method,seed)
 if ~exist('method','var') || isempty(method)
     method = 'pmm';
+end
+if ~exist('seed','var') || isempty(seed)
+    seed = 12345;
 end
 
 if min(sum(~isnan(O),2))==0
@@ -20,5 +26,5 @@ if min(sum(~isnan(O),2))==0
     error('Some rows are only NaN. Remove them e.g. by OmicsRemoveEmptyFeatures.m');
 end
 
-dat = mice(get(O,'data'),method);
+dat = mice(get(O,'data'),method,seed);
 O = set(O,'data',dat,sprintf('MICE imputation using %s.',method));
