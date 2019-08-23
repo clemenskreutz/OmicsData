67187b5a8247b1377962eddc19c309943592a1bd
clemenskreutz <ckreutz@fdm.uni-freiburg.de>
2019-08-23 19:57:50 +0200

    Arbitary x axis

--- Plot/OmicsBoxplotMissings.m
+++ Plot/OmicsBoxplotMissings.m
@@ -1,4 +1,4 @@
-% OmicsBoxplotMissings(O,nbin)
+% OmicsBoxplotMissings(O,[nbin],[option])
 % 
 %   This function produces a boxplot of the proportion of missing values
 %   for the features depending on the median measurment
@@ -7,25 +7,39 @@
 % 
 %   nbin    the number of boxes, i.e. the number of bins for the medians
 % 
+%   option  x-values, e.g. 'mean', 'median'
+% 
 % See also OmicsBoxplotMeanMissings
 
-function OmicsBoxplotMissings(O,nbin)
+function OmicsBoxplotMissings(O,nbin,option)
 if ~exist('nbin','var') || isempty(nbin)
     nbin = 20;
 end
+if ~exist('option','var') || isempty(option)
+    option = 'median';
+end
 
-m = nanmedian(O,2);
+switch(option)
+    case 'median'        
+        x = nanmedian(O,2);
+    case 'mean'        
+        x = nanmean(O,2);
+    otherwise
+%         error('Option %s unknown.',option);
+        x = get(O,option);
+end
+
 antna = sum(isnan(O),2)./get(O,'ns');
 
-[m2,rf] = sort(m);
-antna2 = antna(rf); % sorted according to m
-anzProBin = ceil(length(m)/nbin);
+[m2,rf] = sort(x);
+antna2 = antna(rf); % sorted according to x
+anzProBin = ceil(length(x)/nbin);
 antna2matrix = NaN(anzProBin,nbin);
 
 binnames = cell(1,nbin);
 for i=1:nbin
     ind = (i-1)*anzProBin + (1:anzProBin);
-    ind(ind>length(m)) = []; 
+    ind(ind>length(x)) = []; 
 
     binnames{i} = sprintf('%.2f',nanmean(m2(ind)));
     antna2matrix(1:length(ind),i) = antna2(ind);
@@ -33,7 +47,7 @@ end
 
 boxplot(antna2matrix,'labels',binnames,'labelorientation','inline');
 set(gca,'YGrid','on','LineWidth',1.5,'FontSize',9);
-xlabel('median')
+xlabel(option)
 ylabel('isnan');
 title(strrep(get(O,'name'),'_','\_'));
 
