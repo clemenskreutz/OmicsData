a2ec84496f72195cc169311cfaf31d80a695dcfa
clemenskreutz <ckreutz@fdm.uni-freiburg.de>
2019-08-23 20:02:34 +0200

    Indices remaining in the data as addiontional output

--- OmicsRemoveEmptyFeatures.m
+++ OmicsRemoveEmptyFeatures.m
@@ -5,10 +5,15 @@
 % 
 %   minN    the minmum number
 %           [1] is the default value
+% 
+%           if minN<1, then it is intepreted as fraction, i.e. 0.2 means
+%           available in at least 20% of samples
 
-function O = OmicsRemoveEmptyFeatures(O,minN)
+function [O,drin] = OmicsRemoveEmptyFeatures(O,minN)
 if ~exist('minN','var') || isempty(minN)
     minN = 1;
+elseif minN<1
+    minN = ceil(minN*size(O,2));
 end
 
 dat = get(O,'data');
